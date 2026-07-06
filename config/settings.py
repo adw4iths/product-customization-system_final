76cda050a61c448e46621eb46d70a557e45fb918
Adwaith <adwaithsindhu@gmail.com>
2026-07-06 12:23:12 +0530

    Add STATIC_ROOT

--- config/settings.py
+++ config/settings.py
@@ -61,7 +61,10 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 USE_TZ = True
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+import os
+
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
